e6c9a06441fb10a6c0d9964511d9e8c296a303be
Trisha Prabhu <trishaprabhu@Trishas-MacBook-Pro.local>
2020-12-01 18:19:18 -0600

    Commit!

--- app.R
+++ app.R
@@ -199,10 +199,154 @@ tabPanel("Models",
            br(),
            br(),
            br(),
-           plotOutput(outputId = "readability"))
+           plotOutput(outputId = "readability"))),
+tabPanel("Visualization",
+         titlePanel("Tweet Word Cloud"),
+         sidebarPanel(
+           radioButtons(
+             inputId = "source",
+             label = "Pick a candidate:",
+             choices = c(
+               "Hillary Clinton; 2016" = "hill16",
+               "Donald Trump; 2020" = "don20")
+           ),
+           numericInput("num", "Maximum number of words:",
+                        value = 100, min = 5),
+           colourInput("col", "Background Color:", value = "white"),
+           selectInput(
+             inputId = "language",
+             label = "Remove stopwords (e.g. and, the) in:",
+             choices = c("Danish", "Dutch", "English", "Finnish", 
+                         "French", "German", "Hungarian", "Italian", 
+                         "Norwegian", "Portuguese", "Russian", "Spanish", 
+                         "Swedish"),
+             multiple = FALSE,
+             selected = "English")),
+         mainPanel(wordcloud2Output("cloud")),
+         titlePanel("Character Count"),
+         sidebarPanel(selectInput(inputId = "hist",
+                                  label = "Choose a candidate:",
+                                  choices = c("Hillary Clinton", 
+                                              "Donald Trump"))),
+         mainPanel(plotOutput(outputId = "char"))),
+tabPanel("Discussion",
+         titlePanel("Interpreting the Model"),
+         p("This analysis refers to the Interactive Regression Results
+               displayed on the Models page."),
+         tags$b(p("Approval Rating")),
+         p("The first model regresses Trump's daily Twitter sentiment scores 
+             on his associated daily approval ratings. The median of the 
+             Intercept, -0.554, suggests that at a hypothetical approval rating 
+             of 0, Trump's average sentiment score would be quite negative.
+             It is important to note: the standard error associated with 
+             this value  suggests that the 95% confidence interval is 
+             (-1.17, 0.06), meaning that the true value could be positive, but 
+             even so, barely positive. In other words, we can be fairly sure
+             -- though not entirely sure -- that Trump would have a negative
+             daily Twitter sentiment score at an approval rating of 0 
+             (which, of course, makes sense!). The median of the coefficient
+             on the approval rating variable, 0.0138, suggests that on average,
+             a 1% increase in Trump's daily approval rating results in about a 
+             0.0138 increase in his daily Twitter sentiment score. In other
+             words, his popularity in the public is directly reflected in his
+             Tweets. Once again, the 95% confidence interval cautions us to
+             be wary; indeed, it suggests that the true value could be as
+             low as 0, or as as high as 0.02. We should far from accept these 
+             findings as conclusive; they are not necessarily significant."),
+         tags$b(p("Stock Market")),
+         p("The second model regresses Trump's daily Twitter sentiment scores 
+             on daily stock market opening/closing differences (does a big
+             jump or a big drop affect his sentiment on Twitter?). The median of 
+             the Intercept, 0.05, suggests that at a hypothetical difference
+             value of 0, Trump's average sentiment score would be neutral.
+             Though relatively high, the Intercept's standard error value and its
+             resulting 95% confidence interval -- (0.015, 0.086) -- ultimately leads
+             us to the same conclusion. The median of the coefficient, -0.003, 
+             suggests that, on average, a 1 unit increase in the stock market's 
+             opening/closing difference results in a close to negligible dip in 
+             Trump's daily Twitter sentiment score. In other words, it seems that 
+             the stock market's changes are not a particularly powerful predictor 
+             of Trump's sentiment. This is, once again, qualified by the 
+             standard error/confidence interval. The standard error is very
+             high -- 0.014 -- producing a wide confidence interval of (-0.032,
+             0.027). It's clear, then, that the true value could in fact suggest
+             an important relationship between these two variables. We should,
+             then, take these findings with a grain of salt."),
+         tags$b(p("Interaction")),
+         p("What if we create a model that looks at approval rating, stock 
+               market opening/closing differences, and their interaction?"), 
+         p("This is exactly what the last model aims to do, regressing
+               Trump's daily Twitter sentiment scores on his associated daily
+               approval ratings, the associated daily stock market 
+               opening/closing differences, and their interaction."), 
+         p("The median of the Intercept, -0.494, suggests that at a 
+               hypothetical approval rating of 0% and a hypothetical stock
+               opening/closing difference of 0, Trump's average sentiment
+               score would be relatively negative; this should, however, be taken
+               with a grain of salt, given the high standard error value (0.334).
+               This ultimately implies a 95% confidence interval of 
+               (-1.162, 0.174) -- so the true value could, in fact, represent 
+               a positive sentiment score. (This is similar to the Intercept
+               we saw in the first model.) The median of the coefficient on
+               the approval rating variable suggests that at a hypothetical
+               stock difference value of 0, on average, a 1% increase in Trump's 
+               daily approval rating results in about a 0.0125 increase in his 
+               daily Twitter sentiment score -- a value similar to the first
+               model, but slightly lower. A larger standard error value here
+               suggests that the true value could be as low as -0.003 or as
+               high as 0.003. The median of the coefficient on the range
+               variable suggests that at a hypothetical approval rating of
+               0%, on average, a 1 unit increase in the stock market's 
+             opening/closing difference results in 0.045 increase in Trump's
+             daily Twitter sentiment score. This is quite different from the
+             second model, which implied a neglible dip. In any case, once again,
+             a large standard error value keeps us from striking gold; with a
+               95% confidence interval of (-0.228, 0.318), the true value could be 
+               neglible or a robust increase/decrease."), 
+         titlePanel("About The Data"),
+         p(""), 
+         a("See the data currently in use by visiting this Dropbox link.",
+           
+# At Dan's suggestion, I uploaded my datasets (which were large, and making it
+# impossible for me to commit my work to GitHub) to Dropbox. Also, Dan, 
+# apologies -- the link below was too long to fit within the 80 character code 
+# line limit!
+           
+           href = "https://www.dropbox.com/sh/5azksa5cvrsi9cs/AADvM-p9h8Sqf4oYzcgaMWXda?dl=0")
+),
+tabPanel("About", 
+         titlePanel("About"),
+         h3("Project Background and Motivations"),
+         p("This project aims to explore US President Donald 
+             Trump's Tweets in the months leading up to the 2020 General Election.
+             Unlike his predecessors, Trump has used social media extensively,
+             through which he reaches over 80 million followers on Twitter alone.
+             Given the influence his Tweets have had during his Presidency, 
+             I wanted to better understand what was driving his 
+             behavior (and specifically, his sentiment) on Twitter, and how those
+             patterns compared to those of his 2016 rival, Hillary Clinton.
+             My analysis rests on data sourced from the Trump Twitter Archive, 
+             an open-source database of several U.S. politicians' Tweets, as 
+             well as data from FiveThirtyEight and the CBOE Volatility Index."),
+         h3("About Me"),
+         p("My name is Trisha Prabhu, and I'm a member of Harvard College's
+             Class of 2022. Originally from Naperville, Illinois, at Harvard,
+             I reside in Cabot House. I'm concentrating in Government, on
+             the Tech Science pathway, and pursuing a secondary in Economics. 
+             Within the broad field that is Government, I'm most passionate 
+             about understanding the impact the rise of technology has had on 
+             our society -- specifically, with regards to the way the digital 
+             economy has shaped issues like free speech and privacy -- and 
+             spearheading policy and work to address these challenges. You'll 
+             often find me utilizing data science and quantitative research 
+             methods to dig into this work.
+             You can reach me at trishaprabhu@college.harvard.edu."),
+         a("Visit the GitHub repo for this project here.", 
+           href = "https://github.com/trishprabhu/finalprojectmilestone3-4/tree/main")
 ))
 
-# Define server logic required to draw a histogram
+# Define server logic:
+
 server <- function(input, output) {
   
   datasetInput <- reactive({
@@ -425,8 +569,85 @@ server <- function(input, output) {
     tweetgraph
     
   })
+  
+  data_source <- reactive({
+    
+    if (input$source == "hill16") {
+      data <- hillarytweets$text[1:100]
+    } else if (input$source == "don20") {
+      data <- trumptweets$text[1:100]
+      return(data)
+    }
+    
+  })
+  
+  create_wordcloud <- function(data, num_words = 100, background = "white") {
+    
+    if (is.character(data)) {
+      corpus <- Corpus(VectorSource(data))
+      corpus <- tm_map(corpus, tolower)
+      corpus <- tm_map(corpus, removePunctuation)
+      corpus <- tm_map(corpus, removeNumbers)
+      corpus <- tm_map(corpus, removeWords, stopwords(tolower(input$language)))
+      tdm <- as.matrix(TermDocumentMatrix(corpus))
+      data <- sort(rowSums(tdm), decreasing = TRUE)
+      data <- data.frame(word = names(data), freq = as.numeric(data))
+    }
+    
+    # Make sure a proper num_words is provided:
+    
+    if (!is.numeric(num_words) || num_words < 3) {
+      num_words <- 3
+    }
+    
+    # Grab the top n most common words:
+    
+    data <- head(data, n = num_words)
+    if (nrow(data) == 0) {
+      return(NULL)
+    }
+    
+    wordcloud2(data, backgroundColor = background)
+    
+  }
+  
+  output$cloud <- renderWordcloud2({
+    create_wordcloud(data_source(),
+                     num_words = input$num,
+                     background = input$col)
+    
+  })
+  
+  histInput <- reactive({
+    switch(input$hist,
+           
+           "Hillary Clinton" = hillarytweets,
+           "Donald Trump" = trumptweets)
+  })
+  
+  output$char <- renderPlot({
+    
+    histdataset <- histInput()
+    
+    characterhist <- histdataset %>%
+      ggplot(aes(x = str_length(text))) +
+      geom_histogram(binwidth = 10,
+                     color = "white",
+                     fill = "darkslategray2") +
+      labs(title = "Character Count of Candidate's Tweets",
+           subtitle = "",
+           x = "Character Count",
+           y = "Frequency",
+           caption = "Source: Trump Twitter Archive") +
+      xlim(0, 140) +
+      theme_minimal()
+    
+    characterhist 
+    
+  })
 
 }
 
-# Run the application 
+# Run the application:
+
 shinyApp(ui = ui, server = server)
